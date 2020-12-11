600b78b76044f466b94f2021736f62eb4f724a22
Adam Slack <adamslack@outlook.com>
2020-12-11 12:22:19 +0000

    added some waits to watch it live

--- js/11/2/index.js
+++ js/11/2/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-// const fp = 'test_input.txt'
-const fp = 'input.txt'
+const fp = 'test_input.txt'
+// const fp = 'input.txt'
 
 let layout = fs.readFileSync(fp, 'utf-8')
   .split('\n')
@@ -31,50 +31,65 @@ const scan = (dx, dy, x, y) => {
   return hit
 }
 
-while(hasChanged) {
-//   layout.forEach((line) => {
-//     console.log(JSON.stringify(line))
-//   })
-  // console.log(hasChanged)
-  hasChanged = false
-  for(let y = 0; y < layout.length; y++) {
-    for(let x = 0; x < layout[y].length; x++) {
-      
-      // Determine Surroudings
-      let adjacentOccupancyCount = 0
-      for(let i = -1; i <= 1; i++){
-        for(let j = -1; j <= 1; j++){
-          if(i === 0 && j === 0) {
-            // do nothing
-          }
-          else if (scan(j, i, x, y) === '#') {
-            adjacentOccupancyCount++
-          }
-        }
-      }
-      // Determine Change
-      // if(x===1 && y === 1) console.log(layout[y][x], adjacentOccupancyCount)
-      if(layout[y][x] === 'L' && adjacentOccupancyCount === 0) {
-        nextLayout[y][x] = '#'
-        hasChanged = true
-      }
-      else if(layout[y][x] === '#' && adjacentOccupancyCount >= 5) {
-        nextLayout[y][x] = 'L'
-        hasChanged = true
-      } 
-    }
-  }
-  layout = JSON.parse(JSON.stringify(nextLayout))
-} 
-
-let occupancyCount = 0;
-layout.forEach((line) => {
-  // console.log(JSON.stringify(line))
-  line.forEach((seat) => {
-    if(seat === '#') {
-      occupancyCount++
-    }
-  })
+const wait = (ms) =>  new Promise(res => {
+  setTimeout(res, ms);
 })
 
-console.log(occupancyCount)
+const live = async () => {
+  while(hasChanged) {
+
+    await wait(1000)
+    console.log('\033[2J');
+    layout.forEach((line) => {
+      console.log(line.join())
+    })
+    // console.log(hasChanged)
+    hasChanged = false
+    for(let y = 0; y < layout.length; y++) {
+      for(let x = 0; x < layout[y].length; x++) {
+        
+        // Determine Surroudings
+        let adjacentOccupancyCount = 0
+        for(let i = -1; i <= 1; i++){
+          for(let j = -1; j <= 1; j++){
+            if(i === 0 && j === 0) {
+              // do nothing
+            }
+            else if (scan(j, i, x, y) === '#') {
+              adjacentOccupancyCount++
+            }
+          }
+        }
+        // Determine Change
+        // if(x===1 && y === 1) console.log(layout[y][x], adjacentOccupancyCount)
+        if(layout[y][x] === 'L' && adjacentOccupancyCount === 0) {
+          nextLayout[y][x] = '#'
+          hasChanged = true
+        }
+        else if(layout[y][x] === '#' && adjacentOccupancyCount >= 5) {
+          nextLayout[y][x] = 'L'
+          hasChanged = true
+        } 
+      }
+    }
+    layout = JSON.parse(JSON.stringify(nextLayout))
+  } 
+
+  await wait(1000)
+  console.log('\033[2J');
+  let occupancyCount = 0;
+  layout.forEach((line) => {
+    console.log(line.join())
+    line.forEach((seat) => {
+      if(seat === '#') {
+        occupancyCount++
+      }
+    })
+  })
+
+  await wait(5000)
+
+  console.log('Number of occupied Seats!:', occupancyCount)
+}
+
+live()
