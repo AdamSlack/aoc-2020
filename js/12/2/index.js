509c4172fef47bfea73dfbd975f4521456f41e9a
Adam Slack <adamslack@outlook.com>
2020-12-12 18:41:34 +0000

    cleanup

--- js/12/2/index.js
+++ js/12/2/index.js
@@ -23,30 +23,17 @@ let waypointPos = {
 }
 
 const handlers = {
-  N: (dist) => {
-    waypointPos.y += dist
-  },
-  E: (dist) => {
-    waypointPos.x += dist
-  },
-  S: (dist) => {
-    waypointPos.y -= dist
-  },
-  W: (dist) => {
-    waypointPos.x -= dist
-  },
+  N: (dist) => waypointPos.y += dist,
+  E: (dist) => waypointPos.x += dist,
+  S: (dist) => waypointPos.y -= dist,
+  W: (dist) => waypointPos.x -= dist,
   R: (angle) => {
     const times = angle / 90
     for(let i = 0; i < times; i++) {
-      waypointPos = {
-        x: waypointPos.y,
-        y: waypointPos.x * -1
-      }
+      waypointPos = { x: waypointPos.y, y: waypointPos.x * -1 }
     }
   },
-  L: (angle) => {
-    handlers['R'](360 - angle)
-  },
+  L: (angle) => handlers['R'](360 - angle),
   F: (dist) => {
     shipPos.x += (waypointPos.x * dist)
     shipPos.y += (waypointPos.y * dist)
